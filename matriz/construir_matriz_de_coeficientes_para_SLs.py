825bcf1ab1ea5f3dc878866811e37a7606ad2c2a
Diego Ascanio <diego@turibio.localdomain>
2020-11-06 03:30:15 -0300

    Removendo codigos desenvolvidos fora da metodologia TDD

--- matriz/construir_matriz_de_coeficientes_para_SLs.py
+++
@@ -1,9 +0,0 @@
-def construir_matriz_de_coeficientes(A, m): 
-    matriz_de_indices = np.array([i for i in range(m ** 2)]).reshape(m, m) 
-    coeficientes = np.zeros((m**2, m**2)) 
-    for i in range(m ** 2): 
-        j = int(i/m) 
-        colunas = matriz_de_indices[:,i%m] 
-        for k, l in zip(colunas, range(m)): 
-            coeficientes[i][k] = A[j][l] 
-    return coeficientes
